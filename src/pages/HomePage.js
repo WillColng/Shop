20f5c8142410f2c2ccd0a143858769449966061c
WillColng <61203891+WillColng@users.noreply.github.com>
2024-06-27 12:36:31 +0300

    Content update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,9 +8,10 @@ const HomePage = ({ addToCart }) => {
     <Container>
       <Box my={4}>
         <Typography variant="h2" align="center" gutterBottom>
-          Welcome to GameBoost
+          Welcome to TG referral shop
         </Typography>
         <Typography variant="h5" align="center" paragraph>
+          Here you'll find the best referral deals for various TG games like Hamster Kombat, Blum, Catizen and other incredible projects!
           Boost your gaming experience with our premium referral services. Choose from a variety of packages tailored to your needs and level up your game effortlessly.
         </Typography>
       </Box>
